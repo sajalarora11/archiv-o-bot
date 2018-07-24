b9c7d4f613dec26d5847ae50c07a81b603d3838b
Sajal Arora <sajalarora007@gmail.com>
2018-07-25 01:27:43 +0530

    works just fine

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
-x = [51, 21, 66, 43, 71]
-for i in x:
-    if i < 40:
-        print(i)
-    else:
-        rem = 5-(i%5)
-        res = i + rem
-        if res - i < 3:
-            print(res)
-    
+import mailparser
+import re
+
+# This code removes all the junk text and put mails in an array with just Subject and body per element.
+mails = list()
+body = ""
+with open("2010q2.txt") as f:
+    for line in f:
+        if line.startswith("From "):
+            if body != "":
+                mails.append(body)
+                body = ""
+            continue
+        else:
+            if not any(line.startswith(kw) for kw in ("From:", "To:", "Date", ">", "-", "In-Reply-To:", "References:", "Message-ID:", "\n", "Cc:", "Sent:", "On", "\t", "Hello", "Hi", "Regards", "With Regard", "To unsubscribe", "To change")):
+                body = body + line
+    mails.append(body)
+    print(mails)
+    # The below code uses the package mailparser that simply pulls out the subject and body alone out of emails...
+    for mail in mails:
+        mail = mailparser.parse_from_string(mail)
+        print(mail.body)
