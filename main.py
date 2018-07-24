63f0ce3d112a6a1b1c674564523f40ae32d1f7bc
Sajal Arora <sajalarora007@gmail.com>
2018-07-25 01:21:19 +0530

    Removed most of the junk text and arranged in array of mails

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 with open("2010q2.txt") as f:
     data = {}
     for line in f:
-        if any(line.startswith(kw) for kw in ("From:", "To:", "Date", ">", "-", "In-Reply-To:", "References:", "Message-ID:","From" , "\n")):
+        if any(line.startswith(kw) for kw in ("From:", "To:", "Date", ">", "-", "In-Reply-To:", "References:", "Message-ID:","From" , "\n", "Cc:", "Sent:", "On", "Hello", "Hi", "Regards", "With Regard", "To unsubscribe", "To change")):
             continue
         if line.startswith("Subject:"):
             current_subject = line.split(":")[-1].strip()
