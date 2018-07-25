fc5dadd0fcf6b49efbcb9d124472de8cc73d07e6
Sajal Arora <sajalarora007@gmail.com>
2018-07-25 11:22:30 +0530

    Removed 90% of junk out of mails

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-with open("2010q2.txt") as f:
+import re
+with open("testing.txt") as f:
     data = {}
     for line in f:
-        if any(line.startswith(kw) for kw in ("From:", "To:", "Date", ">", "-", "In-Reply-To:", "References:", "Message-ID:","From" , "\n", "Cc:", "Sent:", "On", "Hello", "Hi", "Regards", "With Regard", "To unsubscribe", "To change")):
+        # Skip the unnecessary lines
+        if any(line.startswith(kw) for kw in ("From ", "From:", "To:", "Date", ">", "-", "In-Reply-To:", "References:", "Message-ID:", "\n", "Cc:", "Sent:", "On", "\t", "Hello", "Hi", "Regards", "With Regard", "To unsubscribe", "To change", "Voice your", "visit the list", "http", "hello", "hi", "hey", "hai", "thanks", "regards", "warm regards", "with regards")):
             continue
+        # removes the Subject from the line...
         if line.startswith("Subject:"):
             current_subject = line.split(":")[-1].strip()
         else:
+            # Append the subject and the body of the email into a dictionary with sight more cleaning...
             data.setdefault(current_subject, "")
+            line = re.sub("\n", " ", line)
+            line = re.sub("\t", "", line)
             data[current_subject] += line
 
 print(data)
